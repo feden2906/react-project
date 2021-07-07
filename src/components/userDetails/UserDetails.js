99727d1d254529f6430e7e973602fff7f2c1727e
feden2906 <feden2906@gmail.com>
2021-07-07 14:12:40 +0300

    user present

--- src/components/userDetails/UserDetails.js
+++ src/components/userDetails/UserDetails.js
@@ -1,7 +1,24 @@
-export default function UserDetails(props) {
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { getUser } from "../../servises/API";
+
+export default function UserDetails() {
+    const {id} = useParams()
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        getUser(id).then(value => setUser(value.data))
+    }, [id])
+    console.log(user)
+
     return (
         <div>
-            user details
+            <div>id: {user?.id}</div>
+            <div>phone: {user?.phone}</div>
+            <div>name: {user?.name}</div>
+            <div>website: {user?.website}</div>
+            <div>city: {user?.address.city}</div>
+            <div>street: {user?.address.street}</div>
         </div>
     );
 }
