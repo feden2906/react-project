6ed7f69d6211a16da99c0fbf13d847415108d344
YevsInna <evseinka@gmail.com>
2021-07-08 11:39:37 +0300

    redux: INC, DEC, RECET

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from 'react-redux'
+
+const NesteChild = () => {
+    const counter = useSelector(({counter: {value}}) => value)
+    const dispatch = useDispatch();
+
+    return (
+        <header className="App-header">
+            <h1>{counter}</h1>
+            <button
+                onClick={() => {
+                    dispatch({type: 'INC'})
+                }
+                }>inc
+            </button>
+
+            <button
+                onClick={() => {
+                    dispatch({type: 'DEC'})
+                }
+                }>dec
+            </button>
+
+            <button
+                onClick={() => {
+                    dispatch({type: 'RECET'})
+                }
+                }>recet
+            </button>
+        </header>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NesteChild/>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,64 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from 'react-redux'
+
+const initialState = {
+    counter: {
+        value: 0
+    },
+    posts: [],
+}
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case 'INC': {
+            return {
+                ...state,
+                counter: {
+                    value: state.counter.value + 1
+                }
+            }
+        }
+
+        case 'DEC': {
+            return {
+                ...state,
+                counter: {
+                    value: state.counter.value - 1
+                }
+            }
+        }
+
+        case 'RECET': {
+            return {
+                ...state,
+                counter: {
+                    value: 0
+                }
+            }
+        }
+        default:
+            return state;
+    }
+}
+const store = createStore(reducer)
+console.log(store.getState())
+const INCREMENT_ACTION = {
+    type: 'INC'
+}
+store.dispatch({
+    type: 'INC',
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
