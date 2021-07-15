d80ac512b179af98e4f119fd590e32ef0f6562dc
feden2906 <feden2906@gmail.com>
2021-07-15 14:19:40 +0300

    fix

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,19 +1,22 @@
 import {useState} from "react";
 
-export default function Form ({users}){
+export default function Form ({users, setChosenUser}){
 
     const [user,setUser]= useState({});
     let userChoose=(e)=>{
-        let user_id = e.target.value;
+        let user_id = +e.target.value;
         let find = users.find(user=> user.id === user_id);
         setUser(find);
+        setChosenUser(find);
     };
     return (
         <div>
             <form onSubmit={userChoose}>
                 <select onChange={userChoose} value={user}>
 
-                    {users.map(value => <option key={value.name} value={value.id}>{value.name}</option>)
+                    <option>Chose person</option>
+                    {
+                        users.map(value => <option key={value.name} value={value.id}>{value.name}</option>)
                     }
                 </select>
             </form>
